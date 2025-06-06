689679b29403f2af98bf4da12b1ebb26e7d78830
Alok <mishraalok189381@gmail.com>
2025-06-06 15:48:28 +0530

    updated the user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,35 +1,48 @@
 import mongoose, { Schema } from "mongoose";
 
 const UserSchema = new Schema({
-
     username: {
+        type: String,
         required: true,
-        type: String
-    },
-    userid: {
-        required: true,
-        unique: true,
-        type: String
+        unique: true, 
+        trim: true, 
+        minlength: 3 
     },
     email: {
+        type: String,
         required: true,
         unique: true,
-        type: String
+        trim: true,
+        match: [/.+@.+\..+/, 'Please fill a valid email address'] // Basic email validation
     },
     password: {
+        type: String,
         required: true,
-        type: String
+        minlength: 6 
     },
-    profilepic: {
-        type: String
-    } ,
-    coverpic: {
-        type: String
+    profilePic: { 
+        type: String,
+        default: 'https://default-profile-pic-url.com/avatar.png' // Default avatar
+    },
+    coverPic: { 
+        type: String,
+        default: 'https://default-cover-pic-url.com/cover.png' // Default cover
     },
     bio: {
-        type: String
-    }
-})
+        type: String,
+        maxlength: 160 
+    },
+    followers: [{ 
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    following: [{ 
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    timestamps: true 
+});
 
 
 const User = mongoose.model('User', UserSchema);
