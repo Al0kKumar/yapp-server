fcfc661001dcfe5626451066c449cbb618d17bf1
Alok <mishraalok189381@gmail.com>
2025-06-06 15:46:56 +0530

    updated the comment model

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -2,29 +2,44 @@ import mongoose, { Schema } from "mongoose";
 
 const CommentSchema = new Schema({
 
-    userId:{
+    userId: {
+        type: Schema.Types.ObjectId, // Reference to the User who made the comment
+        ref: 'User',
         required: true,
-        type: String
     },
-    postId:{
+    postId: {
+        type: Schema.Types.ObjectId, // Reference to the Post the comment belongs to
+        ref: 'Post',
         required: true,
-        type: String
+    },
+    parentCommentId: { // For replies: stores the _id of the parent comment
+        type: Schema.Types.ObjectId,
+        ref: 'Comment',
+        default: null, // Null if it's a top-level comment
     },
     content: {
+        type: String,
+        maxlength: 280, 
+        trim: true
+    },
+    imageUrl: { 
         type: String
     },
-    imageUrl: {
-        type: String
-    },
-    likesCount:{
+    likes: [{ 
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    likesCount: {
         type: Number,
         default: 0
     },
-    replyCount: {
+    replyCount: { 
         type: Number,
         default: 0
-    },
-})
+    }
+}, {
+    timestamps: true 
+});
 
 const Comment = mongoose.model('Comment', CommentSchema);
 export default Comment;
