f91c0bf223c8968d897d3405927726190be1136a
Alok <mishraalok189381@gmail.com>
2025-06-12 16:11:42 +0530

    added the routing and ratelimiter

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,13 @@ import cors from "cors";
 import AuthRoutes from "./routes/auth.routes"
 import { connectDB } from "./config/db";
 import ratelimiter from "./middleware/rate_limiter"
+import postRoutes from './routes/post.routes';
+import likeRoutes from './routes/likes.routes';
+import followRoutes from './routes/follow.routes';
+import notifRoutes from './routes/notification.routes';
+import userRoutes from './routes/user.routes.js';
+import searchRoutes from './routes/search.routes.js';
+
 
 const app = express();
 
@@ -24,7 +31,19 @@ app.get('/health',(req,res) => {
     res.status(200).json({"msg":"all fine"})
 })
 
-app.use('/api', AuthRoutes);
+app.use('/api',userRoutes);
+
+app.use('/api', postRoutes);
+
+app.use('/api',searchRoutes);
+
+app.use('/api',likeRoutes);
+
+app.use('/api', followRoutes);
+
+app.use('/api', notifRoutes);
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT: ${PORT}`);
